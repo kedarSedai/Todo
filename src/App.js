c9a2ad79c9ce3d897b40fad6982117ce5e9efe29
kedar sedai <kedar.sedai05@gmail.com>
2021-05-18 12:01:20 +0545

    Added App component and todoForm component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./App.css";
 
+
+function TodoForm({ addTodo }){
+    const [value, setValue] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!value) return;
+        addTodo(value);
+        setValue("");
+    }
+}
+
+
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn Node.js" },
+    { text: "Learn React.js" },
+    { text: "Learn Java" }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
